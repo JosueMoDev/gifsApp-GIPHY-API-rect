3cb4d29ef0db3a87fbe7a9c73784e3d5f2b8804a
Jonas Morales <jonasjosuemo2021@outlook.com>
2022-12-10 17:17:14 -0600

    features: there are a list what we gotta do

--- src/hooks/useSetUi.js
+++ src/hooks/useSetUi.js
@@ -17,6 +17,8 @@ export const useSetUi = () => {
     const startClosingAlert = () => { 
         dispatch(onCloseAlert());
     }
+    // * hay que trabajer abrir y cerrar flyout menu aqui, hay que mostrar spinner cuando se
+    // * se esta buscando y setando images, limpiar el search tearm cuando se hace click en home
 
     return {
         // * PROPERTIES
