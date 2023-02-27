7b22668806ec46b75775c595ac5660a3e88606f5
JosueMoDev <82752441+JosueMoDev@users.noreply.github.com>
2023-02-27 05:28:49 -0600

    hashRouter implemented

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux'
 import { GifApp } from './GifApp'
 import { store } from './store'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter hashType="noslash" >
         <GifApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
